573ed05ead95ee1237d9d4fd7461a2632199e1c4
FranzOppliger <franzoppliger78@gmail.com>
2021-12-18 15:09:36 -0300

    6 lugares T
    informacion 6 lugares de turismo atacama

--- CinfoTurismo.php
+++ CinfoTurismo.php
@@ -18,16 +18,13 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="css/Estilo.css">
-	<title>Document</title>
+	<title>Lugares</title>
 </head>
-
 <body  class="bg-light">
 	<!--Navbar-->
 	<?php include_once "include/header.php"; ?>
 	<!--Navbar-->
-	  
-	  <!--Slider-->
-	  <div class="container">
+	<div class="container">
 		<div id="myCarousel" class="carousel slide" data-ride="carousel">
 		  <!-- Indicators -->
 		  <ol class="carousel-indicators">
@@ -62,56 +59,424 @@
 		  </a>
 		</div>
 	  </div>
-	<div class="container">
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+	  <div class="container">
+		<center><h1>Bahia Inglesa</h1></center>
+
+	  </div>
+
+	  <!--Slider-->
+	  <div class="container " >
+		<div id="myCarousel2" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
 		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-			<li data-target="#myCarousel" data-slide-to="2"></li>
-			<li data-target="#myCarousel" data-slide-to="3"></li>
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel2" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel2" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel2" data-slide-to="2" style="border-radius: 50px;"></li>
 		  </ol>
 	  
 		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
+		  <div class="carousel-inner" >
 			<div class="item active">
-			  <img src="Fotos/bahia1.jpg" alt="" style="width:100%;">
+			  <img src="Fotos/bahia1.png" alt="Los Angeles" style="border-radius: 50px;">
 			</div>
 	  
 			<div class="item">
-			  <img src="Fotos/bahia2.jpg" alt="" style="width:100%;">
+			  <img src="Fotos/bahia2.png" alt="Chicago" style="border-radius: 50px;">
 			</div>
 		  
 			<div class="item">
-			  <img src="Fotos/bahia3.jpg" alt="" style="width:100%;">
-			</div>
-			<div class="item">
-			  <img src="Fotos/bahia4.jpg" alt="" style="width:100%;">
+			  <img src="Fotos/bahia3.png" alt="New york" style="border-radius: 50px;">
 			</div>
 		  </div>
 	  
 		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		  <a class="left carousel-control" href="#myCarousel2" data-slide="prev" style="border-radius: 50px;" >
 			<span class="glyphicon glyphicon-chevron-left"></span>
 			<span class="sr-only">Previous</span>
 		  </a>
-		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		  <a class="right carousel-control" href="#myCarousel2" data-slide="next" style="border-radius: 50px;">
 			<span class="glyphicon glyphicon-chevron-right"></span>
-			<span class="sr-only">Next</span>  
-
-
-	<div>
-      <h1>Bahia Inglesa</h1>
-      
-
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
 
 
 
-	</div> 
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
 		
+		<div class="card-body">
+		  
+		  <p class="card-text">Bahía Inglesa es un balneario de la costa del Desierto de Atacama en el norte de Chile, conocido por sus playas. Entre ellas, se encuentra la playa La Piscina, una caleta resguardada con formaciones rocosas que encierran aguas claras y tranquilas. La costa ventosa del lado sur hace que las playas Las Machas y Cisne sean sitios populares para realizar deportes acuáticos. Justo al norte del balneario está la ciudad puerto de Caldera, con su Museo Paleontológico.</p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
 
 
-    <!--Footer-->
+
+	<div class="container">
+		<center><h1>Laguna Verde</h1></center>
+
+	  </div>
+
+	  <!--Slider-->
+	  <div class="container " >
+		<div id="myCarousel3" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel3" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel3" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel3" data-slide-to="2" style="border-radius: 50px;"></li>
+		  </ol>
+	  
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" >
+			<div class="item active">
+			  <img src="Fotos/lagunaverde1.png" alt="Los Angeles" style="border-radius: 50px;">
+			</div>
+	  
+			<div class="item">
+			  <img src="Fotos/lagunaverde2.png" alt="Chicago" style="border-radius: 50px;">
+			</div>
+		  
+			<div class="item">
+			  <img src="Fotos/lagunaverde3.png" alt="New york" style="border-radius: 50px;">
+			</div>
+		  </div>
+	  
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel3" data-slide="prev" style="border-radius: 50px;" >
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel3" data-slide="next" style="border-radius: 50px;">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
+
+
+
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
+		
+		<div class="card-body">
+		  
+		  <p class="card-text">Se le encuentra a unos 23 kilómetros al oeste del Paso de San Francisco y por ende a igual distancia de la frontera argentino-chilena, a unos 265 km al este-noreste de la ciudad de Copiapó.<br> El nivel de sus aguas está a 4 350 msnm y la ruta 31-CH contornea su margen sur. El complejo fronterizo chileno San Francisco esta a 111 km y en la zona también se encuentra el parque nacional Nevado Tres Cruces.
+
+                            Este lago está aparentemente desprovisto de vida a causa de la extremada salinidad de sus aguas, las cuales se caracterizan por poseer un bello color verde cerúleo.
+                            Ocupa el fondo de un profundo valle casi circular (circo) rodeado de volcanes siempre cubiertos por nieve, tales como el Nevado Ojos del Salado, El Muerto y el Nevado Incahuasi, entre otros pertenecientes a la cordillera de los Andes. La fauna de la zona es muy escasa debido a la gran aridez y al clima extremo.</p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
+
+
+
+
+	<div class="container">
+		<center><h1>Volcan Ojos del Salado</h1></center>
+
+	  </div>
+
+	  <!--Slider-->
+	  <div class="container " >
+		<div id="myCarousel4" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel4" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel4" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel4" data-slide-to="2" style="border-radius: 50px;"></li>
+		  </ol>
+	  
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" >
+			<div class="item active">
+			  <img src="Fotos/volcan1.png" alt="Los Angeles" style="border-radius: 50px;">
+			</div>
+	  
+			<div class="item">
+			  <img src="Fotos/volcan2.png" alt="Chicago" style="border-radius: 50px;">
+			</div>
+		  
+			<div class="item">
+			  <img src="Fotos/volcan3.png" alt="New york" style="border-radius: 50px;">
+			</div>
+		  </div>
+	  
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel4" data-slide="prev" style="border-radius: 50px;" >
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel4" data-slide="next" style="border-radius: 50px;">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
+
+
+
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
+		
+		<div class="card-body">
+		  
+		  <p class="card-text">El Nevado Ojos del Salado es un estratovolcán perteneciente a la cordillera de los Andes, que se halla enclavado sobre el límite entre Argentina y Chile. Con 6891 m ,es el volcán más alto de la Tierra y la segunda cima de los hemisferios sur y occidental, siendo solo superado por el Aconcagua.</p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
+
+
+
+	<div class="container">
+		<center><h1>Desierto Florido</h1></center>
+
+	  </div>
+
+
+	 <!--Slider-->
+	 <div class="container " >
+		<div id="myCarousel5" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel5" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel5" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel5" data-slide-to="2" style="border-radius: 50px;"></li>
+		  </ol>
+	  
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" >
+			<div class="item active">
+			  <img src="Fotos/florido1.png" alt="Los Angeles" style="border-radius: 50px;">
+			</div>
+	  
+			<div class="item">
+			  <img src="Fotos/florido2.png" alt="Chicago" style="border-radius: 50px;">
+			</div>
+		  
+			<div class="item">
+			  <img src="Fotos/florido3.png" alt="New york" style="border-radius: 50px;">
+			</div>
+		  </div>
+	  
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel5" data-slide="prev" style="border-radius: 50px;" >
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel5" data-slide="next" style="border-radius: 50px;">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
+
+
+
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
+		
+		<div class="card-body">
+		  
+		  <p class="card-text">El desierto florido es un fenómeno que se produce en el desierto de Atacama, el más árido del planeta,consiste en la aparición de una gran diversidad de flores en aquellos años en que las precipitaciones son inusuales y superan el rango normal para el desierto</p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
+
+
+
+
+	<div class="container">
+		<center><h1>Parque Nacional Nevado Tres Cruces</h1></center>
+
+	  </div>
+
+	  <!--Slider-->
+	  <div class="container " >
+		<div id="myCarousel6" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel6" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel6" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel6" data-slide-to="2" style="border-radius: 50px;"></li>
+		  </ol>
+	  
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" >
+			<div class="item active">
+			  <img src="Fotos/parque1.png" alt="Los Angeles" style="border-radius: 50px;">
+			</div>
+	  
+			<div class="item">
+			  <img src="Fotos/parque2.png" alt="Chicago" style="border-radius: 50px;">
+			</div>
+		  
+			<div class="item">
+			  <img src="Fotos/parque3.png" alt="New york" style="border-radius: 50px;">
+			</div>
+		  </div>
+	  
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel6" data-slide="prev" style="border-radius: 50px;" >
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel6" data-slide="next" style="border-radius: 50px;">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
+
+
+
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
+		
+		<div class="card-body">
+		  
+		  <p class="card-text">Este parque esta dividido en dos sectores,esta dividido entre las cordilleras de Domeyko y Claudio Gay.
+			   <br>Desde cualquiera de ellos es posible apreciar la concentración de altas cumbres existentes en el lugar, siendo el Ojos del Salado, el volcán más alto del mundo con 6.879 m.
+
+                   <br>El parque fue concebido como tal con el fin de proteger el ecosistema altoandino y a su escasa, pero interesante, flora y fauna.
+
+                   El sector norte está formado, principalmente, por el salar de Maricunga y la Laguna Santa Rosa. El sur está a mayor altura y la Laguna del Negro Francisco abarca gran parte de su territorio. </p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
+
+
+
+
+	<div class="container">
+		<center><h1>Playa la virgen</h1></center>
+
+	  </div>
+
+	  <!--Slider-->
+	  <div class="container " >
+		<div id="myCarousel7" style="border-radius: 50px;" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators" style="border-radius: 50px;" >
+			<li data-target="#myCarousel7" data-slide-to="0" class="active" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel7" data-slide-to="1" style="border-radius: 50px;"></li>
+			<li data-target="#myCarousel7" data-slide-to="2" style="border-radius: 50px;"></li>
+		  </ol>
+	  
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" >
+			<div class="item active">
+			  <img src="Fotos/playavirgen1.png" alt="Los Angeles" style="border-radius: 50px;">
+			</div>
+	  
+			<div class="item">
+			  <img src="Fotos/playavirgen2.png" alt="Chicago" style="border-radius: 50px;">
+			</div>
+		  
+			<div class="item">
+			  <img src="Fotos/playavirgen3.png" alt="New york" style="border-radius: 50px;">
+			</div>
+		  </div>
+	  
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel7" data-slide="prev" style="border-radius: 50px;" >
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel7" data-slide="next" style="border-radius: 50px;">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+	  </div>
+
+
+
+	  <div class="container mx-auto mt-4">
+		<div class="row">
+		  <div class="col-md-12">
+			<div class="card" style="">
+		
+		<div class="card-body">
+		  
+		  <p class="card-text">Playa La Virgen es de los destinos más apetecidos de la región de Atacama es playa La Virgen, un lugar que pareciera ser caribeño, sin embargo está muy cerca del desierto más seco del mundo, el desierto de Atacama.
+
+                       <br>Su bahía permite lugares cercanos para el camping y establecer zonas de pic nic. Para los amantes de la fotografía, es un lugar de colores que contrastan entre el blanco de la arena, el azul del mar y los cerros secos que la cubren del viento.</p>
+			 
+		  <a href="#" class="btn "><i class="fab fa-github"></i> Mas informacion</a>
+		</div>
+		</div>
+		  </div>    
+			
+
+		
+	  </div>
+	</div>
+
+
+
+	
+
+	  
+	      
+			
+	  
+	  
+		</div>
+		</div>
+	  </div>
+	</div>
+
+	<!--Footer-->
 	<?php include_once "include/footer.php"; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
